46b63a20aef82ff667cb740d13a3cf9a610d56d3
Huseyin Tas <htas767@gmail.com>
2024-03-13 23:33:11 +0300

    init

---
+++ app/src/main/java/com/tashuseyin/calorietracker/ui/theme/Theme.kt
@@ -0,0 +1,48 @@
+package com.tashuseyin.calorietracker.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+
+private val DarkColorScheme = darkColorScheme(
+    primary = BrightGreen,
+    primaryContainer = DarkGreen,
+    secondary = Orange,
+    background = MediumGray,
+    onBackground = TextWhite,
+    surface = LightGray,
+    onSurface = TextWhite,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = BrightGreen,
+    onTertiaryContainer = DarkGreen,
+    secondary = Orange,
+    background = Color.White,
+    onBackground = DarkGray,
+    surface = Color.White,
+    onSurface = DarkGray,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+)
+
+
+@Composable
+fun CalorieTrackerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
