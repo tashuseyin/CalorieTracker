7088804600bb229f69ca1f4ab105859b5e58b172
Hüseyin Tas <huseyin.tas@external.yapikredi.com.tr>
2024-04-07 00:28:26 +0300

    setup shared preferences

---
+++ app/src/main/java/com/tashuseyin/calorietracker/CalorieTrackerApp.kt
@@ -0,0 +1,7 @@
+package com.tashuseyin.calorietracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CalorieTrackerApp: Application()
---
+++ core/src/main/java/com/tashuseyin/core/domain/preferences/Preferences.kt
@@ -0,0 +1,32 @@
+package com.tashuseyin.core.domain.preferences
+
+import com.tashuseyin.core.domain.model.ActivityLevel
+import com.tashuseyin.core.domain.model.Gender
+import com.tashuseyin.core.domain.model.GoalType
+import com.tashuseyin.core.domain.model.UserInfo
+
+interface Preferences {
+    fun saveGender(gender: Gender)
+    fun saveAge(age: Int)
+    fun saveWeight(weight: Float)
+    fun saveHeight(height: Int)
+    fun saveActivityLevel(level: ActivityLevel)
+    fun saveGoalType(type: GoalType)
+    fun saveCarbRatio(ratio: Float)
+    fun saveProteinRation(ratio: Float)
+    fun saveFatRatio(ratio: Float)
+
+    fun loadUserInfo(): UserInfo
+
+    companion object {
+        const val KEY_GENDER = "gender"
+        const val KEY_AGE = "age"
+        const val KEY_WEIGHT = "weight"
+        const val KEY_HEIGHT = "height"
+        const val KEY_ACTIVITY_LEVEL = "activity_level"
+        const val KEY_GOAL_TYPE = "goal_type"
+        const val KEY_CARB_RATIO = "card_ratio"
+        const val KEY_PROTEIN_RATIO = "protein_ration"
+        const val KEY_FAT_RATIO = "fat_ratio"
+    }
+}
